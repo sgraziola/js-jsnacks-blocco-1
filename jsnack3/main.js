76721436b93e89919b4e63e1baec497c6d660f28
sgraziola <simo.fdl@gmail.com>
2022-10-12 12:43:50 +0200

    Snack3: add somma

---
+++ jsnack3/main.js
@@ -0,0 +1,13 @@
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+let userNum;
+let somma = 0;
+for( let i = 1; i <= 10; i++){
+    userNum = Number(prompt(`Inserisci il numero ${i}`));
+    console.log(userNum);
+}
+
+
+
+
+//Il programma stampa la somma di tutti i numeri inseriti.
+
