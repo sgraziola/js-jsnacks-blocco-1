1da62da52a8a8f80bf799044106257b17efa23d0
sgraziola <simo.fdl@gmail.com>
2022-10-12 11:40:54 +0200

    Snack1: add result to DOM

---
+++ jsnacks1/main.js
@@ -0,0 +1,15 @@
+/* Snack 1 */
+//L’utente inserisce due numeri in successione, con due prompt.
+const num1 = prompt("Inserisci il primo numero");
+const num2 = prompt("Inserisci il secondo numero");
+const risultato = document.querySelector("h1")
+//Il software stampa il maggiore. 
+let maxNum;
+if(num1 > num2){
+    maxNum = num1;
+} else {
+    maxNum = num2;
+}
+
+console.log(maxNum);
+risultato.innerHTML= (`il numero maggiore è ${maxNum}`);
