3d4a3ae83a11f0cc21e362ea59ff83f476cb69e1
sgraziola <simo.fdl@gmail.com>
2022-10-13 12:11:18 +0200

    Snack3: Add result to DOM

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -14,14 +14,16 @@ console.log("la somma è " + somma);
 
 
 
-console.log("########### WHILE ##########");
+//console.log("########### WHILE ##########");
 
 let i = 1;
+const sommaEl = document.querySelector("h1");
 while (i <= 10) {
     userNum = Number(prompt(`Inserisci il numero ${i}`));
-    console.log(userNum);
+    //console.log(userNum);
     somma += userNum;
     i++;
 }
 //Il programma stampa la somma di tutti i numeri inseriti.
-console.log("la somma è " + somma);
+sommaEl.innerHTML = (`La somma dei numeri inseriti è ${somma}`);
+//console.log("la somma è " + somma);
