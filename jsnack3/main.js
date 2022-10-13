df49fe8147a9141aa1ae9103b98774eb6c2259d2
sgraziola <simo.fdl@gmail.com>
2022-10-13 12:03:31 +0200

    Snack3: Add loop while

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,16 +1,27 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 let userNum;
 let somma = 0;
-for( let i = 1; i <= 10; i++){
+/* for( let i = 1; i <= 10; i++){
     userNum = Number(prompt(`Inserisci il numero ${i}`));
     console.log(userNum);
     somma += userNum;
 }
 
+//Il programma stampa la somma di tutti i numeri inseriti.
 console.log("la somma è " + somma);
 
+ */
 
 
 
+console.log("########### WHILE ##########");
+
+let i = 1;
+while (i <= 10) {
+    userNum = Number(prompt(`Inserisci il numero ${i}`));
+    console.log(userNum);
+    somma += userNum;
+    i++;
+}
 //Il programma stampa la somma di tutti i numeri inseriti.
-
+console.log("la somma è " + somma);
